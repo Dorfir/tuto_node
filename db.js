6219d8061a9a1f7cb88a40ba4cfd1f1bade3b27c
xav personnel <greendorfir@gmail.com>
2024-05-09 02:58:55 +0200

    et ca continue en gore et en gore

--- db.js
+++ db.js
@@ -1,16 +1,26 @@
 import { Sequelize, DataTypes } from 'sequelize'
 // const sequelize = new Sequelize('sqlite::memory:')
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: ':memory:',
+// const sequelize = new Sequelize({
+//     dialect: 'sqlite',
+//     storage: ':memory:',
+// })
+const sequelize = new Sequelize("node", "root", "", {
+    host: 'localhost',
+    dialect: 'mysql',
 })
 
 const User = sequelize.define('User',  {
     username: DataTypes.STRING,
     birthday: DataTypes.DATE,
 })
+const Carac = sequelize.define('Carac', {
+    strength: DataTypes.INTEGER,
+    dexterity: DataTypes.INTEGER,
+    constitition: DataTypes.INTEGER,
+})
 
 await User.sync({ force: true })
+await Carac.sync({ force: true })
 
 const jane = await User.create({
     username: 'janedoe',
@@ -21,11 +31,20 @@ const xavier = await User.create({
     birthday: new Date(1977, 12, 9),
 })
 
-console.log("Jane's auto-generated ID: ", jane.id)
-console.log("Jane's username: ", jane.username)
+const carac1 = await Carac.create({
+    strength: 12,
+    dexterity: 15,
+    constitition: 13,
+})
 
-console.log("Xavier's auto-generated ID: ", xavier.id)
-console.log("Xavier's username: ", xavier.username)
+// console.log("Jane's auto-generated ID: ", jane.id)
+// console.log("Jane's username: ", jane.username)
 
-const users = await User.findAll();
+// console.log("Xavier's auto-generated ID: ", xavier.id)
+// console.log("Xavier's username: ", xavier.username)
+
+const users = await User.findAll({attributes: [['username', 'name']]});
 console.log('All users: ', JSON.stringify(users, null, 2));
+// console.log(users.every(user => user instanceof User))
+const caracs = await Carac.findAll()
+console.log("Caracs: ", JSON.stringify(caracs, null, 2))
